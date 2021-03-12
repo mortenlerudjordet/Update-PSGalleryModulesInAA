7aa186bedd77afb760fe4054fa932c51911f8cd0
Morten <github@lerun.info>
2021-03-12 14:35:42 +0100

    Fix multiple module versions

--- Update-PSGalleryModulesInAA.ps1
+++ Update-PSGalleryModulesInAA.ps1
@@ -58,13 +58,15 @@ param(
 )
 $VerbosePreference = "silentlycontinue"
 $RunbookName = "Update-PSGalleryModulesInAA"
-Write-Output -InputObject "Starting Runbook: $RunbookName at time: $(get-Date -format r).`nRunning PS version: $($PSVersionTable.PSVersion)`nOn host: $($env:computername)"
+Write-Output -InputObject "Starting Runbook: $RunbookName at time: $(get-Date -format r).`nRunning PS version: $($PSVersionTable.PSVersion)`nOn host: $($env:computername)`nLocale:$((Get-WinSystemLocale).name)"
+
+#region Import Modules
 # Prefer to use Az module if available
 if((Get-Module -Name "Az.Accounts" -ListAvailable) -and (Get-Module -Name "Az.Automation" -ListAvailable) -and (Get-Module -Name "Az.Resources" -ListAvailable))
 {
-    $AccountsModule = Get-Module -Name Az.Accounts -ListAvailable
-    $AutomationModule = Get-Module -Name Az.Automation -ListAvailable
-    $ResourcesModule = Get-Module -Name Az.Resources -ListAvailable
+    $AccountsModule = Get-Module -Name Az.Accounts -ListAvailable | Sort-Object -Unique -Descending -Property Version | Select-Object -First 1
+    $AutomationModule = Get-Module -Name Az.Automation -ListAvailable | Sort-Object -Unique -Descending -Property Version | Select-Object -First 1
+    $ResourcesModule = Get-Module -Name Az.Resources -ListAvailable | Sort-Object -Unique -Descending -Property Version | Select-Object -First 1
 
     Write-Output -InputObject "Running Az.Account version: $($AccountsModule.Version)"
     Write-Output -InputObject "Running Az.Automation version: $($AutomationModule.Version)"
@@ -83,9 +85,9 @@ if((Get-Module -Name "Az.Accounts" -ListAvailable) -and (Get-Module -Name "Az.Au
 }
 elseif((Get-Module -Name AzureRM.Profile -ListAvailable) -and (Get-Module -Name AzureRM.Automation -ListAvailable) -and (Get-Module -Name AzureRM.Resources -ListAvailable))
 {
-    $ProfileModule = Get-Module -Name AzureRM.Profile -ListAvailable
-    $AutomationModule = Get-Module -Name AzureRM.Automation -ListAvailable
-    $ResourcesModule = Get-Module -Name AzureRM.Resources -ListAvailable
+    $ProfileModule = Get-Module -Name AzureRM.Profile -ListAvailable | Sort-Object -Unique -Descending -Property Version | Select-Object -First 1
+    $AutomationModule = Get-Module -Name AzureRM.Automation -ListAvailable | Sort-Object -Unique -Descending -Property Version | Select-Object -First 1
+    $ResourcesModule = Get-Module -Name AzureRM.Resources -ListAvailable | Sort-Object -Unique -Descending -Property Version | Select-Object -First 1
 
     Write-Output -InputObject "Running AzureRM.Profile version: $($ProfileModule.Version)"
     Write-Output -InputObject "Running AzureRM.Automation version: $($AutomationModule.Version)"
@@ -114,6 +116,7 @@ else
 }
 
 $VerbosePreference = "continue"
+#endregion
 
 #region Variables
 $script:ModulesImported = @()
